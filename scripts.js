30ed3ee5ee51f789a81b005f9bcfb6dec30c68e1
Wired Earp <wiredearp@gmail.com>
2021-03-11 17:51:28 +0100

    Farver continue

--- scripts.js
+++ scripts.js
@@ -8,3 +8,19 @@
 (() => {
 	onhashchange = () => !location.hash && (document.querySelector('main').scrollTop = 0);
 })();
+
+document.querySelectorAll('figure').forEach(figure => {
+	setup(figure.querySelectorAll('picture'), figure.querySelectorAll('button'));
+});
+
+/**
+ * @param {NodeList<HTMLPictureElement>} pictures 
+ * @param {NodeList<HTMLButtonElement>} buttons 
+ */
+function setup(pictures, buttons) {
+	buttons.forEach((button, index) => {
+		button.onclick = () => {
+			pictures.forEach((pic, i) => pic.style.display = i === index ? 'block' : 'none');
+		};
+	});
+}
